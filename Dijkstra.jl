03476e839382e3649b8af1134f8724531b156b0d
charliergi <ps3gilles.charlier@gmail.com>
2016-12-07 17:26:28 +0100

    updated

--- Dijkstra.jl
+++ Dijkstra.jl
@@ -1,46 +1,45 @@
+
 #Created By Gilles & Antoine
+
 function notInTheFridge(element,vectorPath)
   isIn = false
   for i=1:length(vectorPath)
     if (element==vectorPath[i])
       isIn = true
-      return isin
+  end
+    return isin
+end
+      
 # function closest
 #
+
 function closest(index,matadj)
   min = typemax(Int64)
   for k=1:n
-    if(matadj[index,k]<min && matadj != 0)# && fonction)
-      min=k # renvoie l'index auquel se trouve le plus petit element de la ligne index
-    end
+    if((matadj[index,k]<min) & (matadj[index,k] != 0) & (notInTheFridge(k,vectorPath)==false))
+      min=u 
+      index=k # renvoie l'index auquel se trouve le plus petit element de la ligne index
+      
   end
+end
+  
 function dijkstra(i,j,matadj)
   #initialisation
   mat = matadj
   n=size(mat,1)
-  infinity = typemax(Int64)
-
   for k=1:n,l=1:n
-    mat[k,l]=infinity
+    mat[k,l]=typemax(Int64)
   end
   mat[i,i]=0
   vectorPath = Int64[]
-  done=false
   #body
   notDone=true
-  #while(notDone)
+  while(notInTheFridge(j,vectorPath)==false)
+     u=
+     push!(vectorPath,closest())
     # verifier si le noeud ajouté se trouve à l'index j
     # si oui : done = true
     # si non : done = false et continuer
 
   #end
 end
-
-#test
-matrice = [1 2 3]
-length(matrice)
-test= dijkstra(1,2,matrice)
-println(test)
-
-vector = Float64[]
-println(vector==Float64[])
