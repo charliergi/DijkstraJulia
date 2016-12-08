656614585d681a4671a06c51021fc13e52bd3056
antoidumont <antoine.dumont@student.uclouvain.be>
2016-12-08 18:30:00 +0100

    agrqze

--- DijkstraOpti.jl
+++ DijkstraOpti.jl
@@ -1,49 +1,50 @@
 #Created By Gilles & Antoine
 # regarde la ligne line de la matrice d'adjacence et renvoie la colonne du plus petit noeud qui n'est pas dans le fridge et >0
-function closest(vector,fridge)
+function closest(vector,fridge,n)
   min = typemax(Int64)
   index=-1
-  for k=1:size(vector,2)
-    if((vector[k]<min) & (in(k,fridge)==false))
-
-      index=k # renvoie l'index auquel se trouve le plus petit element de la ligne index
+  for k=1:n
+    if((vector[k]<min) & (!(in(k,fridge))))
+      index=k                                                                                                                                                                                                                                                                     =k
+      min=vector[k] # renvoie l'index auquel se trouve le plus petit element de la ligne index
     end
   end
   return index
 end
 
-function dijkstra(i,j,matadj)
+function dijkstra(i,j,mat)
   #initialisation
+  n=size(mat,1)
+  matadj = mat
+  for k=1:n,l=1:n
+      if ((k!=l) & (matadj[k,l]==0))
+      matadj[k,l]= typemax(Int64)
+      end
+  end
 
-  vector = Array{Int64,size(matadj,1)}
-  size=size(vector,2)
-  path = Int64[]
-  point=-1
-  for k=1:size
+  vector = Array{Int64}(n)
+  for k=1:n
     vector[k]=typemax(Int64)
   end
   vector[i]=0
-  fridge = Int64[]
-  #body
-  println(k)
-  while(in(j,fridge)==false)
-     u=closest(vector,fridge)
+  fridge = Array{Int64}(0) #body
+
+  while(!(in(j,fridge)))
+     u=closest(vector,fridge,n)
      push!(fridge,u)
-     for v=1:size
-       if in(v,fridge)==false & vector[u]+matadj[u,v]<vector[v]
+     for v=1:n
+       if (!(in(v,fridge)) & vector[u]+matadj[u,v]<vector[v])
          vector[v]=vector[u]+matadj[u,v]
-         point=v
        end
      end
-     if in(point,path)==false
-       push!(path,point)
-     end
   end
-    return vector[end]
+    return vector[j]
 end
 
-p=[0 7 2 8
-7 0 9 1
-2 9 0 3
-8 1 3 0]
-println(dijkstra(1,4,p))
+p=[0 4 2 0 0 0
+4 0 1 5 0 0
+2 1 0 8 10 0
+0 5 8 0 2 6
+0 0 10 2 0 3
+0 0 0 6 3 0]
+dijkstra(1,6,p)
