ec3839c3ff652131e360225cc7fe949a628b967b
antoidumont <antoine.dumont@student.uclouvain.be>
2016-12-10 15:13:07 +0100

    version avec commentaires de Dijkstra

--- DijkstraOpti.jl
+++ DijkstraOpti.jl
@@ -1,56 +1,63 @@
 #Created By Gilles & Antoine
-# regarde la ligne line de la matrice d'adjacence et renvoie la colonne du plus petit noeud qui n'est pas dans le fridge et >0
+
+# fonction qui renvoie l'index de l'éléments le plus petit du tableau vecteur et qui n'est pas dans la pile fridge
 function closest(vector,fridge,n)
   min = Inf
   index=-1
   for k=1:n
     if((vector[k]<min) && (!(in(k,fridge))))
       index=k                                                                                                                                                                                                                                                                     =k
-      min=vector[k] # renvoie l'index auquel se trouve le plus petit element de la ligne index
+      min=vector[k]
     end
   end
   return index
 end
 
+# fonction qui transforme la matrice de telle sorte que la matrice soit bien du type Float64 et que deux noeuds non relié ai une distance égale à l'infini
 function transform(matadj,n)
-  mat=convert(Array{Float64},matadj)
+  mat=convert(Array{Float64},matadj) # convertit la matrice en type "float64"
   for  k=1:n,l=1:n
         if ((k!=l) && (mat[k,l]==0.0))
         mat[k,l] = Inf
-      end
+      end # initialise les valeurs dans la matrice d'adjacence à l'infini si les 2 noeuds ne sont pas relié
   end
   return mat
 end
+
+#= algorithme de Dikkstra
+input: 2 entier: i le noeud de départ et j le noeud d'arrivée
+       un tableau à 2 dimensions: mat qui est la matrice d'adjacence du graphe dont les éléments peuvent être des entiers ou des réels
+output: renvoie une liste de 2 éléments: le premier étant la distance entre les 2 noeuds, le deuxieme étant le chemin parcouru entre les deux noeuds
+=#
 function dijkstra(i,j,mat)
-  #initialisation
-  n=size(mat,1)
-  matadj=transform(mat,n)
+  # initialisation
+  n=size(mat,1) # n= le nombre de noeuds
+  matadj=transform(mat,n) # on crée une nouvelle matrice qui est la même que notre matrice d'adjacence mais avec le bon format
   vector = Array{Float64}(n)
   for k=1:n
     vector[k]=Inf
-  end
-  vector[i]=0
-  fridge = Array{Int64}(0)
-  pred = Array{Int64}(n)
-  #body
+  end # initialisation des labels de tous les noeuds à l'infini
+  vector[i]=0 # label de départ réinitialisé à 0
+  fridge = Array{Int64}(0) # créaton du frigo
+  pred = Array{Int64}(n) # création d'un tableau de dimension n qui sert à retenir les prédécesseurs d'un noeud
+
   while(!(in(j,fridge)))
-     u=closest(vector,fridge,n)
-     push!(fridge,u)
+     u=closest(vector,fridge,n) # initialisation de u au noeud avec la distance minimale et qui n'est pas encore dans le frigo
+     push!(fridge,u) # place le noeud u dans le frigo
      for v=1:n
        if ((!(in(v,fridge))) && (vector[u]+matadj[u,v]<vector[v]))
-         vector[v]=vector[u]+matadj[u,v]
-         pred[v]=u
-
-        end
+         vector[v]=vector[u]+matadj[u,v] # met à jour les labels des noeuds qui ne sont pas dans le frigo
+         pred[v]=u # place ds le tableau les prédécesseurs du noeud u
+       end
      end
   end
-  path = Array{Int64}(0)
-  k=j
+  path = Array{Int64}(0) #création du chemin
+  k=j # initialisation de k au noeud d'arrivée
   while(k != i)
     unshift!(path,k)
     k=pred[k]
-  end
-  unshift!(path,i)
+  end # ajoute au début du tableau chemin les noeuds prédécesseurs de j jusqu'au noeud départ
+  unshift!(path,i) # ajoute finalement au début du tableau chemin le noeud de départ
   return [vector[j],path]
 end
 
