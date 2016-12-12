136d9d073e1347af89ffc8963ee2a7f75a9f09a3
charliergi <ps3gilles.charlier@gmail.com>
2016-12-12 16:37:53 +0100

    =k deleted

--- projet.jl
+++ projet.jl
@@ -6,7 +6,7 @@ function closest(vector,fridge,n)
   index=-1
   for k=1:n
     if((vector[k]<min) && (!(in(k,fridge))))
-      index=k                                                                                                                                                                                                                                                                     =k
+      index=k                                                                                                                                                                                                                                                                     
       min=vector[k]
     end
   end
